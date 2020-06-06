7e8b31a383b3c7f4bd5d536388cec149e9e9bb72
DanielBessadev <danielbessadev@gmail.com>
2020-06-06 15:37:22 -0300

    NLW Start

---
+++ public/scripts/create-point.js
@@ -0,0 +1,91 @@
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+    
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then( res => res.json() )
+    .then( states => {
+        for( state of states){
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+    })
+}
+
+populateUFs()
+
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+    
+    const ufValue = event.target.value
+    
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( cities => {
+        
+        for( city of cities){
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+        }
+        citySelect.disabled = false
+    })
+}
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
+
+// Itens de coleta
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    
+    const itemLi = event.target
+    
+    // Add or remove a HTTP class in JS.
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    console.log('ITEM ID: ', itemId)
+
+    // Verify selected items, and if true, cacth them.. "( item => item == itemId )"
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId  // True or false
+        return itemFound
+    })
+
+    // If already selected, take of selection.
+    if(alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item !=itemId
+            return itemIsDifferent
+        })
+        selectedItems = filteredItems
+    }
+
+    // If not selected, add to selection.
+    else {
+        selectedItems.push(itemId)
+    }
+
+    console.log('selectedItems: ', selectedItems)
+
+    // Refresh hidden input with selected items.
+    collectedItems.value = selectedItems
+}
